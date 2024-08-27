6f8166171ec2565c85e2ca15c4e28829d0f6e520
mahmoodhamdi <hmdy7486@gmail.com>
2024-08-27 16:05:37 +0300

    Set up CI pipeline for Android with Fastlane and Firebase App Distribution

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:selaty/firebase_options.dart';
 import 'package:selaty/selaty.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Selaty());
 }

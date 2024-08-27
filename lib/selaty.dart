8b61c5a581409f1074fcbf361b2901946ed936f3
mahmoodhamdi <hmdy7486@gmail.com>
2024-08-27 12:19:12 +0300

    first commit

---
+++ lib/selaty.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Selaty extends StatelessWidget {
+  const Selaty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Selaty',
+      home: Scaffold(
+        body: Center(
+          child: Text('Selaty'),
+        ),
+      ),
+    );
+  }
+}

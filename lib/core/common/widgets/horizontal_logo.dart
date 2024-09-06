de2f8f2d521b65642e980e93cc350ae9b792a876
mahmoodhamdi <hmdy7486@gmail.com>
2024-09-06 22:47:19 +0300

    Refactor Auth View for Responsive and Adaptive Design

--- lib/core/common/widgets/horizontal_logo.dart
+++ lib/core/common/widgets/horizontal_logo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart'; // Import ScreenUtil
 import 'package:selaty/core/constants/styles.dart';
 
 class HorizontalLogo extends StatelessWidget {
@@ -10,33 +9,44 @@ class HorizontalLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isPortrait =
-        Orientation.portrait == MediaQuery.of(context).orientation;
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        // Logo image with responsive width
         Image.asset(
           'assets/images/logo.png',
-          width: isPortrait ? 100.w : 80.w, // Use ScreenUtil for width
+          width: screenWidth *
+              (isPortrait
+                  ? 0.25
+                  : 0.15), // Width is a percentage of screen width
         ),
         SizedBox(
-          width: 10.h, // Use ScreenUtil for spacing
+          width: screenWidth *
+              0.02, // Spacing is a small percentage of screen width
         ),
         Column(
           children: [
+            // Title with responsive font size
             Text(
               'ســـــــــــلتي',
               style: Styles.textStyle40.copyWith(
-                fontSize: isPortrait
-                    ? 36.sp
-                    : 24.sp, // Ensure text style uses ScreenUtil
+                fontSize: screenWidth *
+                    (isPortrait
+                        ? 0.09
+                        : 0.06), // Font size is a percentage of screen width
               ),
             ),
+            // Subtitle with responsive font size
             Text(
               'S    E    L    A    T    Y',
               style: Styles.textStyle18.copyWith(
-                fontSize: isPortrait
-                    ? 18.sp
-                    : 14.sp, // Ensure text style uses ScreenUtil
+                fontSize: screenWidth *
+                    (isPortrait
+                        ? 0.045
+                        : 0.035), // Font size is a percentage of screen width
                 fontWeight: FontWeight.w900,
               ),
             ),
